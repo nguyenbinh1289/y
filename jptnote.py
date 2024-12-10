37c78c707ba6c73b264a5bed635ca872a77b39e5
nguyenbinh1289 <93067512+nguyenbinh1289@users.noreply.github.com>
2024-12-10 13:59:28 +0700

    Update jptnote.py

--- jptnote.py
+++ jptnote.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 
-subprocess.run(['wget', 'wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 -O cloudflared'
+subprocess.run(['wget', 'wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 -O cloudflared'])
 os.system("python3 -m pip install notebook")
 os.system("chmod +x cloudflared")
 os.system("sudo mv cloudflared /usr/local/bin/")
