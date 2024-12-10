caf55de80889ff9f451074c983dbef76f2cad554
nguyenbinh1289 <93067512+nguyenbinh1289@users.noreply.github.com>
2024-12-10 13:35:44 +0700

    Create main.py

---
+++ .github/workflows/main.py
@@ -0,0 +1,8 @@
+import os
+import subprocess
+
+subprocess.run(['wget', 'wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 -O cloudflared'
+os.system("python3 -m pip install notebook")
+os.system("chmod +x cloudflared")
+os.system("sudo mv cloudflared /usr/local/bin/")
+os.system("cloudflared tunnel --url localhost:8888 & python3 -m notebook --allow-root")
