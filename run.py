a7b289092321c335d1b93e6b5926747147cef8a6
nguyenbinh1289 <93067512+nguyenbinh1289@users.noreply.github.com>
2024-12-11 13:14:50 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,98 +1,86 @@
+#@title **RDP**
+#@markdown  It takes 4-5 minutes for installation
+
 import os
 import subprocess
-import shutil
 
-CRD_SSH_Code = input("Google CRD SSH Code :")
+#@markdown  Visit http://remotedesktop.google.com/headless and copy the command after Authentication
 
+CRP = "" #@param {type:"string"}
+
+#@markdown Enter a Pin (more or equal to 6 digits)
 Pin = 123456 #@param {type: "integer"}
-Autostart = True #@param {type: "boolean"}
 
-class CRDSetup:
+#@markdown Autostart Notebook in RDP
+Autostart = False #@param {type: "boolean"}
+
+
+class CRD:
     def __init__(self, user):
         os.system("apt update")
         self.installCRD()
         self.installDesktopEnvironment()
-        self.changewall()
-        self.installGoogleChrome()
-        self.installTelegram()
-        self.installQbit()
+        self.installGoogleChorme()
         self.finish(user)
+        print("\nRDP created succesfully move to https://remotedesktop.google.com/access")
 
     @staticmethod
     def installCRD():
-        subprocess.run(['wget', 'https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb'])
-        subprocess.run(['dpkg', '--install', 'chrome-remote-desktop_current_amd64.deb'])
-        subprocess.run(['apt', 'install', '--assume-yes', '--fix-broken'])
-        print("Chrome Remoted Desktop Installed !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        print("Installing Chrome Remote Desktop")
+        subprocess.run(['wget', 'https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb'], stdout=subprocess.PIPE)
+        subprocess.run(['dpkg', '--install', 'chrome-remote-desktop_current_amd64.deb'], stdout=subprocess.PIPE)
+        subprocess.run(['apt', 'install', '--assume-yes', '--fix-broken'], stdout=subprocess.PIPE)
 
     @staticmethod
     def installDesktopEnvironment():
+        print("Installing Desktop Environment")
         os.system("export DEBIAN_FRONTEND=noninteractive")
         os.system("apt install --assume-yes xfce4 desktop-base xfce4-terminal")
         os.system("bash -c 'echo \"exec /etc/X11/Xsession /usr/bin/xfce4-session\" > /etc/chrome-remote-desktop-session'")
         os.system("apt remove --assume-yes gnome-terminal")
         os.system("apt install --assume-yes xscreensaver")
-        os.system("sudo apt purge light-locker")
-        os.system("sudo apt install --reinstall xfce4-screensaver")
         os.system("systemctl disable lightdm.service")
-        print("Installed XFCE4 Desktop Environment !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
     @staticmethod
-    def installGoogleChrome():
-        subprocess.run(["wget", "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"])
-        subprocess.run(["dpkg", "--install", "google-chrome-stable_current_amd64.deb"])
-        subprocess.run(['apt', 'install', '--assume-yes', '--fix-broken'])
-        print("Google Chrome Installed !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    
-    @staticmethod
-    def installTelegram():
-        subprocess.run(["apt", "install", "--assume-yes", "telegram-desktop"])
-        print("Telegram Installed !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    def installGoogleChorme():
+        print("Installing Google Chrome")
+        subprocess.run(["wget", "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"], stdout=subprocess.PIPE)
+        subprocess.run(["dpkg", "--install", "google-chrome-stable_current_amd64.deb"], stdout=subprocess.PIPE)
+        subprocess.run(['apt', 'install', '--assume-yes', '--fix-broken'], stdout=subprocess.PIPE)
 
     @staticmethod
-    def changewall():
-        os.system(f"curl -s -L -k -o xfce-verticals.png https://images.hdqwalls.com/download/eagle-ai-art-xh-1366x768.jpg")
-        current_directory = os.getcwd()
-        custom_wallpaper_path = os.path.join(current_directory, "xfce-verticals.png")
-        destination_path = '/usr/share/backgrounds/xfce/'
-        shutil.copy(custom_wallpaper_path, destination_path)
-        print("Wallpaper Changed !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-   
-    @staticmethod
-    def installQbit():
-        subprocess.run(["sudo", "apt", "update"])
-        subprocess.run(["sudo", "apt", "install", "-y", "qbittorrent"])
-        print("Qbittorrent Installed !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-           
-            
+    def finish(user):
+        print("Finalizing")
+        if Autostart:
+            os.makedirs(f"/home/{user}/.config/autostart", exist_ok=True)
+            link = "https://colab.research.google.com/github/PradyumnaKrishna/Colab-Hacks/blob/master/Colab%20RDP/Colab%20RDP.ipynb"
+            colab_autostart = """[Desktop Entry]
+Type=Application
+Name=Colab
+Exec=sh -c "sensible-browser {}"
+Icon=
+Comment=Open a predefined notebook at session signin.
+X-GNOME-Autostart-enabled=true""".format(link)
+            with open(f"/home/{user}/.config/autostart/colab.desktop", "w") as f:
+                f.write(colab_autostart)
+            os.system(f"chmod +x /home/{user}/.config/autostart/colab.desktop")
+            os.system(f"chown {user}:{user} /home/{user}/.config")
+
         os.system(f"adduser {user} chrome-remote-desktop")
-        command = f"{CRD_SSH_Code} --pin={Pin}"
+        command = f"{CRP} --pin={Pin}"
         os.system(f"su - {user} -c '{command}'")
         os.system("service chrome-remote-desktop start")
         
-        print("..........................................................") 
-        print(".....Brought By NIGGa................................") 
-        print("..........................................................") 
-        print("......#####...######...####....####...##.......####.......") 
-        print("......##..##....##....##......##..##..##......##..##......")  
-        print("......##..##....##.....####...######..##......######......") 
-        print("......##..##....##........##..##..##..##......##..##......") 
-        print("......#####...######...####...##..##..######..##..##......") 
-        print("..........................................................") 
-        print("..Youtube Video Tutorial - https://youtu.be/xqpCQCJXKxU ..") 
-        print("..........................................................") 
-        print("Log in PIN : 123456") 
-        print("User Name : user") 
-        print("User Pass : root") 
-        while True:
-            pass
+
+        print("Finished Succesfully")
+
 
 try:
-    if CRD_SSH_Code == "":
+    if CRP == "":
         print("Please enter authcode from the given link")
     elif len(str(Pin)) < 6:
         print("Enter a pin more or equal to 6 digits")
     else:
-        CRDSetup(username)
+        CRD(username)
 except NameError as e:
     print("'username' variable not found, Create a user first")
