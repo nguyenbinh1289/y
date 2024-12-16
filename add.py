b8c8969daa545ecc6fc9309dec3139f9c4555116
nguyenbinh1289 <93067512+nguyenbinh1289@users.noreply.github.com>
2024-12-16 20:14:29 +0700

    Update add.py

--- add.py
+++ add.py
@@ -1,12 +1,16 @@
-import gdown
-
-# URL file Google Drive (thay đổi từ dạng "view" sang "uc" để gdown hoạt động)
-url = "https://drive.google.com/uc?id=1-3J0YK2dAL0pDD-95FvqRZX--RgUkFBd"
-
-# Đường dẫn lưu file sau khi tải
-output = "/mnt/linux-lite5.2-64bit.zip"  # Đường dẫn đầy đủ nơi lưu file
-
-# Tải file
-gdown.download(url, output, quiet=False)
-
-print(f"File đã được tải về và lưu tại: {output}")
+print("##########################################################")
+    print("#                                                        #")
+    print("#                     \||/                               #")
+    print("#                     |  @___oo                          #")
+    print("#           /\  /\   / (__,,,,|                          #")
+    print("#          ) /^\) ^\/ _)                qemuGithub!      #")
+    print("#          )   /^\/   _)                CoDeD By:        #")
+    print("#          )   _ /  / _)                        UNKNOW   #")
+    print("#      /\  )/\/ ||  | )_)                                #")
+    print("#     <  >      |(,,) )__)                               #")
+    print("#      ||      /    \)___)\                              #")
+    print("#      | \____(      )___) )___                          #")
+    print("#      \______(_______;;; __;;;                          #")
+    print("#                                                        #")
+    print("##########################################################")
+    print("DONE!")
