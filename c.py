f0a40d1a8c7f4a973c275648b68938178936060c
nguyenbinh1289 <93067512+nguyenbinh1289@users.noreply.github.com>
2024-12-16 12:04:03 +0700

    Update c.py

--- c.py
+++ c.py
@@ -1,1 +1,12 @@
+import gdown
 
+# URL file Google Drive
+url = "https://drive.google.com/uc?id=1WlzOMBtITNea7X8Qu-EIo0rErRq4SAg_"
+
+# Đường dẫn lưu file sau khi tải
+output = "/mnt/win10.zip"  # Đổi tên file tùy ý
+
+# Tải file
+gdown.download(url, output, quiet=False)
+
+print(f"File đã được tải về và lưu tại: {output}")
