c50a09862885ed4a5f49a49a233b6255641d5e9a
nguyenbinh1289 <93067512+nguyenbinh1289@users.noreply.github.com>
2024-12-19 18:38:17 +0700

    Update c.py

--- c.py
+++ c.py
@@ -1,10 +1,10 @@
 import gdown
 
 # URL file Google Drive
-url = "https://drive.google.com/uc?id=1z7pmIXtKvDMMlIa66nQqwrYoAy7iJUCW"
+url = "https://drive.google.com/uc?id=1j6tn7iacA-cqjRvbMVodz0WQyMdsfqg9"
 
 # Đường dẫn lưu file sau khi tải
-output = "/mnt/win2012r2.zip"  # Đổi tên file tùy ý
+output = "/mnt/winXP.zip"  # Đổi tên file tùy ý
 
 # Tải file
 gdown.download(url, output, quiet=False)
