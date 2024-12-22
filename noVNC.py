b8777f201bbac18aecfd9d3a601600cc6e7ea985
nguyenbinh1289 <93067512+nguyenbinh1289@users.noreply.github.com>
2024-12-22 11:48:01 +0700

    Update noVNC.py

--- noVNC.py
+++ noVNC.py
@@ -1,15 +1,19 @@
 import os
 
 Token = input("NGROK Authtoken :")
-print ("Installing NGROK")
+print ("Installing NGROK...")
 os.system("wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz")
 os.system("sudo tar -xvf ngrok-v3-stable-linux-amd64.tgz")
 os.system("sudo mv ngrok /usr/local/bin/")
 os.system("clear")
-print("Installing noVNC")
+print("Installing noVNC...")
 os.system("git clone https://github.com/novnc/noVNC.git")
 os.system("ngrok config add-authtoken {Token}")
 Port = input("Set port cho VNC :")
 Http = input("Set port cho NGROK :")
-print ("connect to VNC :")
-print ("ngrok http {Http} & ./noVNC/utils/novnc_proxy --listen {Port}")
+print ("code for vnc_proxy :")
+print ("./noVNC/utils/novnc_proxy --listen {Your Port}")
+print ("!Lưu ý: Nhập lệnh vnc ở trang khác.")
+
+
+
