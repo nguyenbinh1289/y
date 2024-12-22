009b5f56f38517065e6609b30c5bdec22886b131
nguyenbinh1289 <93067512+nguyenbinh1289@users.noreply.github.com>
2024-12-22 12:42:51 +0700

    Update ware.sh

--- ware.sh
+++ ware.sh
@@ -4,8 +4,22 @@ read -p "Yes/No : " user_choice
 
 if [ "$user_choice" -eq Yes ]; then
   clear
-  
+  curl -s -l -o "novncng.py" "https://github.com/nguyenbinh1289/y/raw/main/NGrok%26novnc.py"
+  python novncng.py
+elif [ "$user_choice" -eq No ]; then
+  clear
+  exit 1
 else
-     echo "Error404. Vui lòng chạy lại script."
-     exit 1
+  echo "Error404. chose again"
+
+  read -p "Y/N : " answer
+
+if [ "$answer" -eq Y ]; then
+  clear
+  sudo ./ware.sh
+elif [ "$answer" -eq N ]; then
+  clear
+  exit 1
 fi
+
+
