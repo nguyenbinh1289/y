45b3f85ea125274d2077ac8fa24a42e38c554ab8
nguyenbinh1289 <93067512+nguyenbinh1289@users.noreply.github.com>
2024-12-22 15:57:04 +0700

    Update ware.sh

--- ware.sh
+++ ware.sh
@@ -6,7 +6,7 @@ if [ "$user_choice" -eq 1 ]; then
   clear
   wget -O "ngNovnc.py" "https://raw.githubusercontent.com/nguyenbinh1289/y/main/NGrok&novnc.py"
   clear
-  python3 novncng.py
+  python3 ngNovnc.py
 elif [ "$user_choice" -eq 2 ]; then
   clear
   exit 1
