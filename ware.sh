53a8a022ad2f1ece56879afb4b24567f00a1d9b7
nguyenbinh1289 <93067512+nguyenbinh1289@users.noreply.github.com>
2024-12-22 15:52:00 +0700

    Update ware.sh

--- ware.sh
+++ ware.sh
@@ -1,25 +1,17 @@
 echo "Do you want to continue? :"
 
-read -p "Yes/No : " user_choice
+read -p "1.Yes/2.No : " user_choice
 
-if [ "$user_choice" -eq Yes ]; then
+if [ "$user_choice" -eq 1 ]; then
   clear
-  curl -s -l -o "novncng.py" "https://github.com/nguyenbinh1289/y/raw/main/NGrok%26novnc.py"
-  python novncng.py
-elif [ "$user_choice" -eq No ]; then
+  wget -O "ngNovnc.py" "https://raw.githubusercontent.com/nguyenbinh1289/y/main/NGrok&novnc.py"
+  clear
+  python3 novncng.py
+elif [ "$user_choice" -eq 2 ]; then
   clear
   exit 1
 else
   echo "Error404. chose again"
-
-  read -p "Y/N : " answer
-
-if [ "$answer" -eq Y ]; then
-  clear
-  sudo ./ware.sh
-elif [ "$answer" -eq N ]; then
-  clear
-  exit 1
 fi
 
 
