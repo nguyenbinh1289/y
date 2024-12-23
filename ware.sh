f9fb77ed23aaaaa25c85c17ec6ac50670579d3eb
nguyenbinh1289 <93067512+nguyenbinh1289@users.noreply.github.com>
2024-12-23 18:10:51 +0700

    Update ware.sh

--- ware.sh
+++ ware.sh
@@ -4,15 +4,14 @@ read -p "1.Yes/2.No : " user_choice
 
 if [ "$user_choice" -eq 1 ]; then
   clear
-  wget -O "ngNovnc.py" "https://raw.githubusercontent.com/nguyenbinh1289/y/main/NGrok&novnc.py"
-  clear
+  curl -s -L -o ngNovnc.py "https://raw.githubusercontent.com/nguyenbinh1289/y/main/NGrok&novnc.py"
   python3 ngNovnc.py
 elif [ "$user_choice" -eq 2 ]; then
   clear
   exit 1
 else
   echo "Error404. chose again"
-  wget https://github.com/nguyenbinh1289/y/raw/main/addNo.sh && chmod +x addNo.sh
+  curl -s -L -o addNo.sh "https://github.com/nguyenbinh1289/y/raw/main/addNo.sh" && chmod +x addNo.sh
   sudo ./addNo.sh
 fi
 
