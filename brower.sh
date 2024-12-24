42751d34afa9bacb9d81c8bce2aa7afcf6c5dc83
nguyenbinh1289 <93067512+nguyenbinh1289@users.noreply.github.com>
2024-12-24 11:46:52 +0700

    Update brower.sh

--- brower.sh
+++ brower.sh
@@ -8,3 +8,4 @@ Pin: origin packages.mozilla.org
 Pin-Priority: 1000
 ' | sudo tee /etc/apt/preferences.d/mozilla
 sudo apt-get update && sudo apt-get install firefox
+gpg -n -q --import --import-options import-show /etc/apt/keyrings/packages.mozilla.org.asc | awk '/pub/{getline; gsub(/^ +| +$/,""); if($0 == "35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3") print "\nThe key fingerprint matches ("$0").\n"; else print "\nVerification failed: the fingerprint ("$0") does not match the expected one.\n"}'
