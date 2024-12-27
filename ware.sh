e1e13b53c49644697274c743200c9c5887118979
nguyenbinh1289 <93067512+nguyenbinh1289@users.noreply.github.com>
2024-12-27 22:10:44 +0700

    Update ware.sh

--- ware.sh
+++ ware.sh
@@ -3,14 +3,18 @@ echo "Do you want to continue? :"
 read -p "Yes/No : " user_choice
 
 case $user_choice in
-[yY][eE][sS]
+[yY][eE][sS])
   clear
   curl -s -L -o ngNovnc.py "https://raw.githubusercontent.com/nguyenbinh1289/y/main/NGrok&novnc.py"
   python3 ngNovnc.py
-case $user_choice in
-[nN][oO]
+  ;;
+  [nN][oO])
   clear
   exit 1
+  ;;
+  *)
+  ;;
+  esac
 else
   echo "Error404. chose again"
   curl -s -L -o addNo.sh "https://github.com/nguyenbinh1289/y/raw/main/addNo.sh" && chmod +x addNo.sh
