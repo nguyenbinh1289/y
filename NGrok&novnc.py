97d60cb942296e0ba4021285e4b68d16352d1a61
nguyenbinh1289 <93067512+nguyenbinh1289@users.noreply.github.com>
2024-12-29 21:50:12 +0700

    Update NGrok&novnc.py

--- NGrok&novnc.py
+++ NGrok&novnc.py
@@ -1,5 +1,5 @@
 import os
 
 Port = input("Set port cho VNC and NGROK :")
-Link_vnc:  = "Link_vnc"
-os.system("ngrok http {Port} & {Link_vnc}")
+Link_vnc:  = "Link"
+os.system("ngrok http {Port} & {Link}")
