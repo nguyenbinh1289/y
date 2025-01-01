1c43a07c98efa81b117aaf5838d812283c978581
nguyenbinh1289 <93067512+nguyenbinh1289@users.noreply.github.com>
2025-01-01 22:41:02 +0700

    Update jptnote.py

--- jptnote.py
+++ jptnote.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 
-Authtoken = input("NGORK AUTHTOKEN :")
+Authtoken = input("NGORK AUTHTOKEN: ")
 os.system("wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz")
 os.system("tar -xvf ngrok-v3-stable-linux-amd64.tgz")
 os.system("sudo mv ngrok /usr/local/bin/")
