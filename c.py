4e9124617c3ec7bc0e8c09019d0d0289e0cd1cf8
nguyenbinh1289 <93067512+nguyenbinh1289@users.noreply.github.com>
2025-01-04 22:16:43 +0700

    Update c.py

--- c.py
+++ c.py
@@ -1,10 +1,10 @@
 import gdown
 
 # URL file Google Drive
-url = "https://drive.google.com/uc?id=1zI4C8oRRP7YlWZEWYySgUn0aVUWQhafJ"
+url = "https://drive.google.com/uc?id=1zRgd8Piv5r7aVmrg8aBLlnGdebE8eqUG"
 
 # Đường dẫn lưu file sau khi tải
-output = "/mnt/win2k12r2.zip"  # Đổi tên file tùy ý
+output = "/mnt/win10lite.rar"  # Đổi tên file tùy ý
 
 # Tải file
 gdown.download(url, output, quiet=False)
