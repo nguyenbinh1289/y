fe2ea57edcc8c8a8f118240b85b97a5a210d154b
nguyenbinh1289 <93067512+nguyenbinh1289@users.noreply.github.com>
2025-01-05 22:17:58 +0700

    Update noVNC.py

--- noVNC.py
+++ noVNC.py
@@ -1,20 +1,15 @@
-import os
+#INstalling NGROK
 
-os.system("curl -s -L -o add.sh https://github.com/nguyenbinh1289/y/raw/main/add.sh && chmod +x add.sh && sudo ./add.sh")
-Token = input("NGROK Authtoken: ")
-print ("Installing NGROK...")
-os.system("wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz")
-os.system("sudo tar -xvf ngrok-v3-stable-linux-amd64.tgz")
-os.system("sudo mv ngrok /usr/local/bin/")
-os.system("clear")
-print("Installing noVNC...")
-os.system("git clone https://github.com/novnc/noVNC.git")
-os.system("ngrok config add-authtoken {Token}")
-Port = input("Set port cho vnc: ")
-print ("code for vnc_proxy: ")
-print ("./noVNC/utils/novnc_proxy --listen {Your Port}")
-print ("!Lưu ý: Nhập lệnh vnc ở trang khác.")
-os.system("curl -s -L -o ware.sh https://github.com/nguyenbinh1289/y/raw/main/ware.sh")
-os.system("chmod +x ware.sh")
-os.system("sudo ./ware.sh")
+sudo apt update && curl -L "https://github.com/nguyenbinh1289/y/raw/main/add.sh"
+read -p "NGROk Authtoken: " Token
+clear
+wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz && sudo tar -xvf ngrok-v3-stable-linux-amd64.tgz && sudo mv ngrok /usr/local/bin/
+clear
+#Install novnc
+
+git clone https://github.com/novnc/noVNC.git && ngrok config add-authtoken "$Token"
+read -p "Set port cho NGROK;noVNC :" Port
+clear
+ngrok http "$Port" & ./noVNC/utils/novnc_proxy --listen "$Port"
+
 
