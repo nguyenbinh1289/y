d9e9def2b4dcc0ec68205d7e959355f87f69d2a1
nguyenbinh1289 <93067512+nguyenbinh1289@users.noreply.github.com>
2025-01-05 20:24:46 +0700

    Update c.py

--- c.py
+++ c.py
@@ -4,7 +4,7 @@ import gdown
 url = "https://drive.google.com/uc?id=1zRgd8Piv5r7aVmrg8aBLlnGdebE8eqUG"
 
 # Đường dẫn lưu file sau khi tải
-output = "/mnt/win10lite.rar"  # Đổi tên file tùy ý
+output = "/workspace/muk/win10lite.rar"  # Đổi tên file tùy ý
 
 # Tải file
 gdown.download(url, output, quiet=False)
