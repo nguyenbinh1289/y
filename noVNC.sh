27034d61f95f6440d72d9dac034e788506da8c9c
nguyenbinh1289 <93067512+nguyenbinh1289@users.noreply.github.com>
2025-01-06 12:17:45 +0700

    Update noVNC.sh

--- noVNC.sh
+++ noVNC.sh
@@ -1,7 +1,6 @@
 #INstalling NGROK
 
-sudo apt update && clear && curl -L "https://github.com/nguyenbinh1289/y/raw/main/add.sh" && wget https://github.com/nguyenbinh1289/y/raw/main/ware.sh
-chmod +x ware.sh && sudo ./ware.sh && clear
+sudo apt update && clear && curl -L "https://github.com/nguyenbinh1289/y/raw/main/add.sh"
 read -p "NGROk Authtoken: " Token
 clear
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz && sudo tar -xvf ngrok-v3-stable-linux-amd64.tgz && sudo mv ngrok /usr/local/bin/
@@ -9,7 +8,7 @@ clear
 #Install novnc
 
 git clone https://github.com/novnc/noVNC.git && ngrok config add-authtoken "$Token"
-read -p "Set port cho NGROK;noVNC :" Port
+read -p "Set port cho NGROK;noVNC: " Port
 clear
 ngrok http "$Port" & ./noVNC/utils/novnc_proxy --listen "$Port"
 
