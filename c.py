f24a4f66a8b1a2c14a6e9f825ff8053dccb782d4
nguyenbinh1289 <93067512+nguyenbinh1289@users.noreply.github.com>
2025-03-22 20:56:26 +0700

    Update c.py

--- c.py
+++ c.py
@@ -4,7 +4,7 @@ import gdown
 url = "https://drive.google.com/uc?id=1t7UXFe86NzjGTpwWIcQsQz_2Nrdp52Ly"
 
 # Đường dẫn lưu file sau khi tải
-output = "win10lite.iso"  # Đổi tên file tùy ý
+output = "win2019ltsc.zip"  # Đổi tên file tùy ý
 
 # Tải file
 gdown.download(url, output, quiet=False)
