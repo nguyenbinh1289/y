3920c8b6a94dc34467fce50b78e28496f99fc737
nguyenbinh1289 <93067512+nguyenbinh1289@users.noreply.github.com>
2025-03-25 20:12:17 +0700

    Update cdst.sh

--- cdst.sh
+++ cdst.sh
@@ -121,6 +121,7 @@ fi
 
 #Starting Qemu
 clear
+qemu-img resize /mnt/a.qcow2 +100G > /dev/null
 echo "Đang khởi chạy máy ảo..."
 echo "Đã khởi động VM thành công vui lòng tự cài ngrok và mở cổng 5900(use novnc)"
 
