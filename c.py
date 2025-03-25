a443b9f65fc76ec1f2723543a70b457833abea50
nguyenbinh1289 <93067512+nguyenbinh1289@users.noreply.github.com>
2025-03-25 22:16:46 +0700

    Update c.py

--- c.py
+++ c.py
@@ -1,7 +1,7 @@
 import gdown
 
 # URL file Google Drive
-url = "https://drive.google.com/uc?id=13TX80UNJQUcVwg1ullq2tfSmqji_vBHe"
+url = "https://drive.google.com/uc?id=1FZSJJFKjn7uxP1AZmIzq4zt27MZ1HNpJ"
 
 # Đường dẫn lưu file sau khi tải
 output = "/mnt/winwork.iso"  # Đổi tên file tùy ý
