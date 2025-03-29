033597720af9c1ad483f736d72cc4db7bedee61c
nguyenbinh1289 <93067512+nguyenbinh1289@users.noreply.github.com>
2025-03-29 21:19:27 +0700

    Update cdst.sh

--- cdst.sh
+++ cdst.sh
@@ -51,21 +51,11 @@ fi
 
 # Hiển thị menu lựa chọn hệ điều hành
 echo "Chọn hệ điều hành để chạy VM:"
-echo "1. Windows 10/FastBoot"
-echo "2. Windows 11/FastBoot"
 echo "3.SL11(24H2)/FastBoot*"
 echo "4.ForWork/FastBoot*"
 
 read -p "Nhập lựa chọn của bạn : " user_choice
 
-if [ "$user_choice" -eq 1 ]; then
-    echo "Bạn đã chọn Windows 10."
-    file_url="https://github.com/jshruwyd/discord-vps-creator/raw/refs/heads/main/a.py"
-    file_name="a.py"
-elif [ "$user_choice" -eq 2 ]; then
-    echo "Bạn đã chọn Windows 11."
-    file_url="https://github.com/jshruwyd/discord-vps-creator/raw/refs/heads/main/b.py"
-    file_name="b.py"
 elif [ "$user_choice" -eq 3 ]; then
     if [ ! -e /mnt/a.iso ]; then
        echo "Downloading..."
@@ -81,10 +71,6 @@ if [ ! -s /mnt/a.iso ]; then
     rm -f "/mnt/a.iso"
     exit 1
 fi
-    
-#Run Qemu-Kvm
-sudo kvm -cpu host,+topoext,hv_relaxed,hv_spinlocks=0x1fff,hv-passthrough,+pae,+nx,kvm=on,+svm -smp sockets=1,cores=4,threads=2 -M q35,usb=on -device usb-tablet -m 8G,slots=4,maxmem=16G -mem-prealloc -mem-path /dev/hugepages -device virtio-balloon-pci -vga virtio -net nic,netdev=n0,model=virtio-net-pci -netdev user,id=n0,hostfwd=tcp::3389-:3389 -boot c -device virtio-serial-pci -device virtio-rng-pci -enable-kvm -drive file=/dev/"$DL",format=raw,if=none,id=nvme0 -device nvme,drive=nvme0,serial=deadbeaf1,num_queues=8 -monitor stdio -drive if=pflash,format=raw,readonly=off,file=/usr/share/ovmf/OVMF.fd -uuid e47ddb84-fb4d-46f9-b531-14bb15156336 -vnc :0 -drive file=driver.iso,media=cdrom -drive file=a.iso,media=cdrom -vnc :0
-exit
   
 elif [ "$user_choice" -eq 4 ]; then
     if [ ! -e /workspaces/action/gdown!.py ]; then
@@ -97,75 +83,38 @@ elif [ "$user_choice" -eq 4 ]; then
 
     # Kiểm tra file ISO có thực sự tải được không
     pip install gdown && python3 gdown!.py
-if [ ! -s /mnt/winwork.iso ]; then
-    echo "Error: ISO file is empty or corrupted!"
-    rm -f "/mnt/winwork.iso"
-    exit 1
-fi
-    
-#Run Qemu-Kvm
-    sudo kvm -cpu host,+topoext,hv_relaxed,hv_spinlocks=0x1fff,hv-passthrough,+pae,+nx,kvm=on,+svm -smp sockets=1,cores=4,threads=2 -M q35,usb=on -device usb-tablet -m 8G -device virtio-balloon-pci -vga virtio -net nic,netdev=n0,model=virtio-net-pci -netdev user,id=n0,hostfwd=tcp::3389-:3389 -boot c -device virtio-serial-pci -device virtio-rng-pci -enable-kvm -drive file=/dev/"$DL",format=raw,if=none,id=nvme0 -device nvme,drive=nvme0,serial=deadbeaf1,num_queues=8 -monitor stdio -drive if=pflash,format=raw,readonly=off,file=/usr/share/ovmf/OVMF.fd -uuid e47ddb84-fb4d-46f9-b531-14bb15156336 -drive file=/mnt/driver.iso,media=cdrom -drive file=/mnt/winwork.iso,media=cdrom -vnc :0
-    exit
-    
-# Tải file Python
-sleep 5
-echo "Đang tải file $file_name từ $file_url..."
-wget -O "/mnt/$file_name" "$file_url"
-clear
-if [ $? -ne 0 ]; then
-    echo "Lỗi khi tải file. Vui lòng kiểm tra kết nối mạng hoặc URL."
-    exit 1
-fi
-
-# Cài đặt gdown và chạy file Python
-echo "Đang cài đặt gdown và chạy file $file_name..."
-pip install gdown
-python3 "/mnt/$file_name"
-clear
-if [ $? -ne 0 ]; then
-    echo "Lỗi khi chạy file Python. Vui lòng kiểm tra lại."
-    exit 1
-fi
-
-# Chờ 3 phút sau khi chạy file Python
-echo "Chờ 5s trước khi tiếp tục..."
-sleep 5
-
-# Giải nén các file .zip trong thư mục /mnt
-echo "Đang giải nén tất cả các file .zip trong /mnt..."
-unzip '/mnt/*.zip' -d /mnt/
-clear
-if [ $? -ne 0 ]; then
-    echo "Lỗi khi giải nén file. Vui lòng kiểm tra lại file tải về."
-    exit 1
-fi
-
+    if [ ! -s /mnt/winwork.iso ]; then
+       echo "Error: ISO file is empty or corrupted!"
+       rm -f "/mnt/winwork.iso"
+       exit 1
+    fi
+      
 #Starting Qemu
-clear
-qemu-img resize /mnt/a.qcow2 +100G
 sleep 3
 echo "Đang khởi chạy máy ảo..."
 echo "Đã khởi động VM thành công vui lòng tự cài ngrok và mở cổng 5900(use novnc)"
 
-
- sudo kvm \
-    -daemonize \
-    -cpu host,+topoext,hv_relaxed,hv_spinlocks=0x1fff,hv-passthrough,+pae,+nx,kvm=on,+svm \
-    -smp sockets=1,cores=4,threads=2 \
-    -M q35,usb=on \
-    -device usb-tablet \
-    -m 8G
-    -device virtio-balloon-pci \
-    -vga virtio \
-    -net nic,netdev=n0,model=virtio-net-pci \
-    -netdev user,id=n0,hostfwd=tcp::3389-:3389 \
-    -boot c \
-    -device virtio-serial-pci \
-    -device virtio-rng-pci \
-    -enable-kvm \
-    -drive file=/mnt/a.qcow2 \
-    -drive if=pflash,format=raw,readonly=off,file=/usr/share/ovmf/OVMF.fd \
-    -uuid e47ddb84-fb4d-46f9-b531-14bb15156336 \
-    -soundhw hda \
-    -vnc :0
+sudo kvm \
+-cpu host,+topoext,hv_relaxed,hv_spinlocks=0x1fff,hv-passthrough,+pae,+nx,kvm=on,+svm \
+-smp sockets=1,cores=4,threads=2 \
+-M q35,usb=on \
+-device usb-tablet \
+-m 8G \
+-device virtio-balloon-pci \
+-vga virtio \
+-net nic,netdev=n0,model=virtio-net-pci \
+-netdev user,id=n0,hostfwd=tcp::3389-:3389 \
+-boot c \
+-device virtio-serial-pci \
+-device virtio-rng-pci \
+-enable-kvm \
+-drive file=/dev/"$DL",format=raw,if=none,id=nvme0 \
+-device nvme,drive=nvme0,serial=deadbeaf1,num_queues=8 \
+-monitor stdio \
+-drive if=pflash,format=raw,readonly=off,file=/usr/share/ovmf/OVMF.fd \
+-uuid e47ddb84-fb4d-46f9-b531-14bb15156336 \
+-drive file=/mnt/driver.iso,media=cdrom \
+-drive file=/mnt/winwork.iso,media=cdrom \
+-vnc :0
+exit
 fi
